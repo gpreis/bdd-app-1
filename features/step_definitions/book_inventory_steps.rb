70df5a2c3fdf9af75f5af3cd72afeb4443ce045c
Gabriel Reis <gbrielpedot@gmail.com>
2018-06-03 17:54:19 -0300

    Add books to the inventory

--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -14,3 +14,19 @@ Then("I should see the list of my books") do
   expect(page).to have_content("Don Quixote")
   expect(page).to have_content("Dom Casmurro")
 end
+
+When("I submit a new book to my inventory") do
+  click_link "New Book"
+
+  fill_in "book_name", with: "Meditations"
+  fill_in "book_author", with: "Marcus Aurelius"
+
+  click_button "Create Book"
+end
+
+Then("I should see the new book in my inventory") do
+  visit books_path
+
+  expect(page).to have_content("Meditations")
+  expect(page).to have_content("Marcus Aurelius")
+end
